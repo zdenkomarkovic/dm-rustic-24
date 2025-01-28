da9216641a4c7242f0451072965412ad50c87430
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-01-28 18:47:00 +0100

    .

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Logo from "../public/logo.jpg";
 import Link from "next/link";
-import { ChevronDownIcon, MenuIcon, PhoneIcon } from "lucide-react";
+import { MenuIcon, PhoneIcon } from "lucide-react";
 
 import {
   Sheet,
@@ -12,12 +12,6 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
 
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
@@ -69,36 +63,6 @@ const MobileMenu = () => (
 const DesktopNav = () => (
   <ul className="hidden gap-8 lg:flex text-white text-xl">
     {navList.map((item, index) => {
-      if (item.list)
-        return (
-          <HoverCard key={index} openDelay={0} closeDelay={50}>
-            <HoverCardTrigger>
-              <motion.div
-                whileHover={{ color: "hsl(var(--primary))", scale: 1.1 }}
-                className="flex gap-1 "
-              >
-                {item.title}
-                <ChevronDownIcon className="w-[18px]" />
-              </motion.div>
-            </HoverCardTrigger>
-            <HoverCardContent className="p-0">
-              {item.list.map((link, index2) => (
-                <motion.li
-                  key={`${index}.${index2}`}
-                  whileHover={{
-                    backgroundColor: "hsl(var(--primary))",
-                  }}
-                  className="rounded-md"
-                >
-                  <Link className="px-2 py-2 block" href={link.link}>
-                    {link.title}
-                  </Link>
-                </motion.li>
-              ))}
-            </HoverCardContent>
-          </HoverCard>
-        );
-
       return (
         <Link key={index} href={item.link}>
           <motion.li
