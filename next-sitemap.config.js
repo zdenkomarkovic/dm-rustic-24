cdd20ce8df1636c62c8313aa17013fdbb98a0248
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-02-06 23:29:47 +0100

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.dekorativneciglice.rs/",
+  generateRobotsTxt: true, // Automatski generiše robots.txt
+  sitemapSize: 5000, // Opcionalno: ograničava broj URL-ova po fajlu
+};
